c07be7a30e69077e9c91bc42b9ab5b45d23e9d98
Nguyen Dinh An <ngdinhan210@gmail.com>
2025-10-27 20:17:09 +0700

    change: headless

--- src/crawler/selenium_utils.py
+++ src/crawler/selenium_utils.py
@@ -58,7 +58,7 @@ def get_driver():
 
 
     chrome_options = Options()
-    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
