70ea1b62f3b484ba15c5b9658da666c2825136c8
Nguyen Dinh An <ngdinhan210@gmail.com>
2025-11-05 16:39:47 +0700

    update: crawler 5-11

--- src/crawler/selenium_agoda_hotel_detail.py
+++ src/crawler/selenium_agoda_hotel_detail.py
@@ -1,3 +1,4 @@
+from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,21 +7,28 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 from selenium_utils import *
 
 
 def crawl_hotel_detail(file_path):
     driver = get_driver()
     with open(file_path, "r", encoding="utf-8") as f:
-        region = f.readline().strip().replace("# Region: ","")
         while True:
             line = f.readline()
             if not line:
                 break
 
-            hotel_id = line.strip().split(",")[0].strip()
-            hotel_link = line.strip().split(",")[1].strip()
+            hotel_id = line.strip().split(" ")[0].strip()
+            hotel_link = line.strip().split(" ")[1].strip()
+            #---------------- Sửa thông tin ngày trong link ----------------#
+            parsed_url = urlparse(hotel_link)
+
+            query_params = parse_qs(parsed_url.query)
+            query_params["checkIn"] = datetime.strftime(datetime.now()+ timedelta(days=2),r"%Y-%m-%d")
+            query_params["checkOut"] = datetime.strftime(datetime.now() + timedelta(days=3),r"%Y-%m-%d")
+            modified_query = urlencode(query_params, doseq=True)
+            hotel_link = urlunparse(parsed_url._replace(query=modified_query))
 
 
             #-------------------- Lấy chi tiết khách sạn --------------------#
@@ -33,34 +41,33 @@ def crawl_hotel_detail(file_path):
 
             try:
 
-                # #Scroll to end page to load all content
-                # last_height = driver.execute_script("return document.body.scrollHeight")
-                # while True:
-                #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                #     time.sleep(1)  # Chờ nội dung tải
-                #     new_height = driver.execute_script("return document.body.scrollHeight")
-                #     if new_height == last_height:
-                #         break
-                #     last_height = new_height
-
-
                 hotel_name = None
                 hotel_address = None
                 room_types = []
 
                 try:
-                    hotel_name_element = safe_find_element(driver, 'div[class="HeaderCerebrum"] > div:nth-child(1) > h1')
+                    # kiểm tra nếu tồn tại div[class="RoomGrid-searchTimeOutText"] thì bỏ qua
+                    timeout_element = safe_find_element(driver, 'div[class="RoomGrid-searchTimeOutText"]')
+                    if timeout_element:
+                        print(f"Timeout while searching for rooms in hotel ID {hotel_id}. Skipping...")
+                        continue
+
+
+
+                    hotel_name_element = safe_find_element(driver, 'h1[data-selenium*= "hotel-header-name"]')
                     if hotel_name_element:
                         hotel_name = hotel_name_element.get_attribute("innerText").strip()
 
-                    hotel_address_element = safe_find_element(driver,'div[class="HeaderCerebrum"] > div:nth-child(2) > span')
+                    hotel_address_element = safe_find_element(driver,'span[data-selenium*="hotel-address-map"]')
                     if hotel_address_element:
                         hotel_address = hotel_address_element.get_attribute("innerText").strip()
 
                     rooms_types_elements = safe_find_elements_many(driver,'div[class="MasterRoom"]')
+                    print('Found room types elements: ', len(rooms_types_elements))
                     for room_type_element in rooms_types_elements:
                         #Loai phong
-                        room_type_name_element = safe_find_element(room_type_element,'div[class="MasterRoom-header"] > div[class*="MasterRoom-headerLeft"] > h4 > button[data-selenium="MasterRoom-headerTitle"] > p > div[class = "MasterRoom-headerTitle--text"] > div:nth-child(1) > span[data-selenium="masterroom-title-name"]')
+                        # room_type_name_element = safe_find_element(room_type_element,'div[class="MasterRoom-header"] > div[class*="MasterRoom-headerLeft"] > h4 > button[data-selenium="MasterRoom-headerTitle"] > p > div[class = "MasterRoom-headerTitle--text"] > div:nth-child(1) > span[data-selenium="masterroom-title-name"]')
+                        room_type_name_element = safe_find_element(room_type_element,'span[data-selenium="masterroom-title-name"]')
                         if room_type_name_element:
                             room_type_name = room_type_name_element.get_attribute("innerText").strip()
 
@@ -78,12 +85,25 @@ def crawl_hotel_detail(file_path):
 
                         #Gia theo tung option
                         price_option_list = []
-                        price_option_list_elements = safe_find_elements_many(room_type_element,'div[class*="MasterRoom-roomsList"] > div > div[class*="ChildRoomsList-room"]')
+                        # price_option_list_elements = safe_find_elements_many(room_type_element,'div[class*="MasterRoom-roomsList"] > div > div[class*="ChildRoomsList-room"]')
+                        price_option_list_elements = safe_find_elements_many(room_type_element,'div[class*="MasterRoom"] div[class*="ChildRoomsList-room"][data-element-name = "child-room-item"]')
+
+                        if len(price_option_list_elements) == 0:
+                            while True:
+                                print("Scrolling to load more price options...")
+                                driver.execute_script("window.scrollBy(0, 600);")
+                                time.sleep(1)
+                                price_option_list_elements = safe_find_elements_many(room_type_element,'div[class*="MasterRoom"] div[class*="ChildRoomsList-room"][data-element-name = "child-room-item"]')
+                                if len(price_option_list_elements) > 0 or is_end_of_page(driver):
+                                    break
+
+                        print("Found price option elements: ", len(price_option_list_elements))
                         for price_option_element in price_option_list_elements:
                             #Lay gia theo tung option
                             price_option_price = None
                             # price_option_price_element = safe_find_element(price_option_element,'div[class="ChildRoomsList-room-contents"] > div[class="ChildRoomsList-roomCell ChildRoomsList-roomCell-price relativeCell"] > div[class="ChildRoom__PriceContainer"] > div[class="PriceContainer-Top"] > div > div[class="PriceContainer"] > div > div > span[data-selenium="PriceDisplay"] > span:nth-child(2) > strong')
-                            price_option_price_element = safe_find_element(price_option_element,'div[class*="ChildRoomsList-room-contents"] > div[class*="ChildRoomsList-roomCell ChildRoomsList-roomCell-price relativeCell"] > div[class*="ChildRoom__PriceContainer"] > div[class*="PriceContainer-Top"] > div > div[class*="PriceContainer"] > div > div > span[data-selenium="PriceDisplay"]')
+                            # price_option_price_element = safe_find_element(price_option_element,'div[class*="ChildRoomsList-room-contents"] > div[class*="ChildRoomsList-roomCell ChildRoomsList-roomCell-price relativeCell"] > div[class*="ChildRoom__PriceContainer"] > div[class*="PriceContainer-Top"] > div > div[class*="PriceContainer"] > div > div > span[data-selenium="PriceDisplay"]')
+                            price_option_price_element = safe_find_element(price_option_element,'span[data-selenium="PriceDisplay"]')
                             
 
 
@@ -94,7 +114,8 @@ def crawl_hotel_detail(file_path):
                             
                             #Lay tung option
                             price_option_name = None
-                            price_option_name_element = safe_find_element(price_option_element,'div[class="ChildRoomsList-room-contents"] > div[class="ChildRoomsList-roomCell ChildRoomsList-roomCell-capacity"] > div > div > button')
+                            # price_option_name_element = safe_find_element(price_option_element,'div[class="ChildRoomsList-room-contents"] > div[class="ChildRoomsList-roomCell ChildRoomsList-roomCell-capacity"] > div > div > button')
+                            price_option_name_element = safe_find_element(price_option_element,'button[data-selenium="ChildRoomsList-capacity-container"]')
                             if price_option_name_element:
                                 print("Found price option name element")
                                 price_option_name = price_option_name_element.get_attribute("aria-label").strip()
@@ -111,6 +132,7 @@ def crawl_hotel_detail(file_path):
                 print(f"Error accessing hotel link {hotel_link}: {e}")
             finally:
                 driver.quit()
+                print("DUng lại driver")
 
 
 
@@ -118,7 +140,7 @@ def crawl_hotel_detail(file_path):
 
 
 if __name__ == "__main__":
-    file_path = r"src/crawler/output/test.txt"  # Thay đổi đường dẫn tới file của bạn
+    file_path = r"data/test.csv"  # Thay đổi đường dẫn tới file của bạn
 
     crawl_hotel_detail(file_path)
 
